8108a144b53819d90a16a593a00ada6cd09aa177
artinramizi11 <artin11@outlook.com>
2024-07-09 15:12:29 +0200

    fake store

---
+++ assigments/javascript/fake store/script.js
@@ -0,0 +1,36 @@
+const container = document.querySelector('.container');
+const idNumber = document.querySelector('.idNumber');
+const productDetails = document.querySelector('.productDetails')
+
+fetch('https://fakestoreapi.com/products')
+    .then(res=>res.json())
+    .then(data => createProducts(data))
+    
+
+function createProducts(data){
+    data.forEach(function(product) {
+        container.innerHTML += `
+             <div class="card" onclick="showProductId(${product.id})">
+                <img src="${product.image}" width="150px">
+   <p class="descriptions">${product.description}</p>
+   <p>Price: ${product.price}</p>
+   <p>Rating: ${product.category}</p>
+                </div>
+        `
+    })
+}
+
+function showProductId(id){
+    fetch(`https://fakestoreapi.com/products/${id}`)
+            .then(res=>res.json())
+            .then(function(data) {
+                productDetails.innerHTML = `
+                <div class="selected">
+                 <h1>ID: ${data.id}</h1>
+                 <p>title: ${data.title}</p> 
+                </div>
+                
+                `
+            });
+            
+}   
---
+++ assigments/javascript/highestRatedMovie/movies.js
@@ -0,0 +1,32 @@
+let movies = [
+    {
+        name: 'The Godfather',
+        releaseYear: 1972,
+        rating: 9.2,
+        mainActors: ['Marlon Brando', 'Al Pacino', 'James Caan']
+    },
+    {
+        name: 'The Shawshank Redemption',
+        releaseYear: 1994,
+        rating: 9.3,
+        mainActors: ['Tim Robbins', 'Morgan Freeman', 'Bob Gunton']
+    },
+    {
+        name: 'Pulp Fiction',
+        releaseYear: 1994,
+        rating: 8.9,
+        mainActors: ['John Travolta', 'Uma Thurman', 'Samuel L. Jackson']
+    },
+    {
+        name: 'The Dark Knight',
+        releaseYear: 2008,
+        rating: 9.0,
+        mainActors: ['Christian Bale', 'Heath Ledger', 'Aaron Eckhart']
+    },
+    {
+        name: 'Forrest Gump',
+        releaseYear: 1994,
+        rating: 8.8,
+        mainActors: ['Tom Hanks', 'Robin Wright', 'Gary Sinise']
+    }
+];
